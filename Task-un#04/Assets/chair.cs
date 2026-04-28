93c29d2e11910cb063512c9611a5ece8499257e5
miho2007 <mzhaviamikheil@gmail.com>
2026-04-28 22:29:31 +0400

    Table interaction distance upgrade file change

---
+++ Task-un#04/Assets/chair.cs
@@ -0,0 +1,56 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class chair : UdonSharpBehaviour
+{
+    public float pushDistance = 0.2f;
+    public float smooth = 6f;
+
+    private Vector3 targetPos;
+    private bool moving;
+    private bool cooldown;
+
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (moving)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                targetPos,
+                Time.deltaTime * smooth
+            );
+
+            if (Vector3.Distance(transform.position, targetPos) < 0.01f)
+            {
+                moving = false;
+            }
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (cooldown) return;
+
+        cooldown = true;
+
+        Vector3 dir = (transform.position - player.GetPosition()).normalized;
+        dir.y = 0;
+
+        targetPos = transform.position + dir * pushDistance;
+        moving = true;
+
+        SendCustomEventDelayedSeconds(nameof(Reset), 0.3f);
+    }
+
+    public void Reset()
+    {
+        cooldown = false;
+    }
+}
