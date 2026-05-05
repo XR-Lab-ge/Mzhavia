3ee4d4e656e5efce65f1ac5480a986730806152f
miho2007 <mzhaviamikheil@gmail.com>
2026-05-05 22:23:53 +0400

    Test-un01

---
+++ Test-un01/Assets/chair.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class chair : UdonSharpBehaviour
+{
+    public VRC.SDK3.Components.VRCStation station;
+
+    public override void Interact()
+    {
+        Debug.Log("Interact fired");
+
+        if (station == null)
+        {
+            Debug.Log("Station is NULL");
+            return;
+        }
+
+        var player = Networking.LocalPlayer;
+
+        if (player == null)
+        {
+            Debug.Log("No local player");
+            return;
+        }
+
+        Debug.Log("Forcing station use");
+        station.UseStation(player);
+    }
+}
---
+++ Test-un01/Assets/door.cs
@@ -0,0 +1,40 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class door : UdonSharpBehaviour
+{
+    public Animator animator;
+
+    private bool isOpen = false;
+    private bool busy = false;
+
+    public override void Interact()
+    {
+        if (busy) return;
+
+        busy = true;
+
+        isOpen = !isOpen;
+
+        animator.ResetTrigger("Open");
+        animator.ResetTrigger("Close");
+
+        if (isOpen)
+        {
+            animator.SetTrigger("Open");
+        }
+        else
+        {
+            animator.SetTrigger("Close");
+        }
+
+        SendCustomEventDelayedSeconds(nameof(ResetBusy), 0.2f);
+    }
+
+    public void ResetBusy()
+    {
+        busy = false;
+    }
+}
