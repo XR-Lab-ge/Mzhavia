ccec59295ecf209d51f880cb1105a3b67e0f9fef
miho2007 <mzhaviamikheil@gmail.com>
2026-05-05 22:23:53 +0400

    Test-un01

---
+++ Test-un01/Assets/table.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class table : UdonSharpBehaviour
+{
+    public Renderer tableRenderer;
+
+    public override void Interact()
+    {
+        Color newColor = new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f)
+        );
+
+        tableRenderer.material.color = newColor;
+    }
+}
