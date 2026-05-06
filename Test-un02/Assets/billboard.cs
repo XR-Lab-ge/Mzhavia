0dc1f6526651a4891c8cee12000a9f81a3401ac4
miho2007 <mzhaviamikheil@gmail.com>
2026-05-06 17:36:49 +0400

    Test 02 upload

---
+++ Test-un02/Assets/billboard.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class billboard : UdonSharpBehaviour
+{
+    void LateUpdate()
+    {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+        if (localPlayer != null)
+        {
+            // Makes the object look at the player's head
+            transform.LookAt(localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position);
+
+            // UI usually faces backwards after a LookAt, so flip it 180 degrees
+            transform.Rotate(0, 180, 0);
+        }
+    }
+}
---
+++ Test-un02/Assets/chair.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class chair : UdonSharpBehaviour
+{
+    public VRC.SDK3.Components.VRCStation station;
+
+    // We create a custom public method for the UI
+    public void _SitInChair()
+    {
+        Debug.Log("UI Button Clicked");
+
+        if (station == null)
+        {
+            Debug.Log("Station is NULL");
+            return;
+        }
+
+        VRCPlayerApi player = Networking.LocalPlayer;
+
+        if (player == null) return;
+
+        // Forces the player into the station
+        station.UseStation(player);
+    }
+
+    // Optional: Keep this if you want BOTH the button and the physical chair to work
+    public override void Interact()
+    {
+        _SitInChair();
+    }
+}
